fd522cca78b2739f86e754de00e3dfdf2017d0a6
FloresLaureano <107213843+FloresLaureano@users.noreply.github.com>
2026-08-30 17:53:42 -0300

    Devolver errores de validacion como 400

--- src/main/java/ar/edu/uade/inversorar/business/ReglaNegocioException.java
+++ src/main/java/ar/edu/uade/inversorar/business/ReglaNegocioException.java
@@ -1,5 +1,8 @@
 package ar.edu.uade.inversorar.business;
 
+import jakarta.ejb.ApplicationException;
+
+@ApplicationException(rollback = true)
 public class ReglaNegocioException extends RuntimeException {
     public ReglaNegocioException(String message) { super(message); }
 }
